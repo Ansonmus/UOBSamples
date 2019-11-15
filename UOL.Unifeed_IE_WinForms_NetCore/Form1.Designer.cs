4ea882491c19315cdbf23a3ea406866fd07c42d9
Joost Roza <joost@ttt.nl>
2019-11-15 09:51:50 +0100

    BBAUOBINFR-14 .NET Core port of the windows forms app.

---
+++ UOL.Unifeed_IE_WinForms_NetCore/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace UOL.Unifeed_IE_WinForms_NetCore
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.browser = new System.Windows.Forms.WebBrowser();
+			this.logBox = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.browser);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.logBox);
+			this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+			this.splitContainer1.SplitterDistance = 266;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// browser
+			// 
+			this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.browser.Location = new System.Drawing.Point(0, 0);
+			this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+			this.browser.Name = "browser";
+			this.browser.Size = new System.Drawing.Size(800, 266);
+			this.browser.TabIndex = 0;
+			this.browser.Navigating += Browser_Navigating;
+			this.browser.Navigated += Browser_Navigated;
+			// 
+			// logBox
+			// 
+			this.logBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+			| System.Windows.Forms.AnchorStyles.Left)
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.logBox.Location = new System.Drawing.Point(3, 3);
+			this.logBox.Multiline = true;
+			this.logBox.Name = "logBox";
+			this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.logBox.Size = new System.Drawing.Size(794, 174);
+			this.logBox.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.splitContainer1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Unifeed Demo";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.WebBrowser browser;
+		private System.Windows.Forms.TextBox logBox;
+	}
+}
+
---
+++ UOL.Unifeed_IE_WinForms_NetCore/Form1.cs
@@ -0,0 +1,164 @@
+﻿namespace UOL.Unifeed_IE_WinForms_NetCore
+{
+	using System;
+	using System.Collections.Specialized;
+	using System.Web;
+	using System.Windows.Forms;
+	using Newtonsoft.Json;
+
+	public partial class Form1 : Form
+	{
+		public const string ClientId = "2BA_DEMOAPPS_PKCE";
+
+#if BETA
+		public const string AuthorizeBaseUrl = "https://uol-auth.beta.2ba.nl";
+		public const string UnifeedBaseUrl = "https://uol-unifeed.beta.2ba.nl";
+		public const string ApiBaseUrl = "https://uol-api.beta.2ba.nl/1";
+#else
+		public const string AuthorizeBaseUrl = "https://uol-auth.2ba.nl";
+		public const string UnifeedBaseUrl = "https://uol-unifeed.2ba.nl";
+		public const string ApiBaseUrl = "https://uol-api.2ba.nl/1";
+#endif
+
+		public const string UnifeedSchemeName = "nl.2ba.uol";
+		public static readonly string AuthorizeUrl = $"{AuthorizeBaseUrl}/OAuth/Authorize";
+		public static readonly string AuthorizeTokenUrl = $"{AuthorizeBaseUrl}/OAuth/Token";
+		public static readonly string AuthorizeListenerAddress = $"http://localhost:43215/"; // Must end with slash
+		public static readonly string AuthorizeHookUrl = $"{AuthorizeListenerAddress}tokenreceiver"; // = Redirect_uri as configured for client
+		public static readonly string UnifeedHookUrl = $"{UnifeedSchemeName}://request";
+
+		private SharedCode.Authentication.OAuthToken _currentToken = null;
+
+		public Form1()
+		{
+			InitializeComponent();
+			browser.ScriptErrorsSuppressed = false;
+		}
+
+		private async void Form1_Load(object sender, EventArgs e)
+		{
+			Log($"Form loaded. starting authenticate");
+			var authService = new SharedCode.Authentication.Authentication(new SharedCode.Authentication.AuthenticationConfig()
+			{
+				AuthorizeUrl = AuthorizeUrl,
+				AuthorizeHookUrl = AuthorizeHookUrl,
+				AuthorizeListenerAddress = AuthorizeListenerAddress,
+				AuthorizeTokenUrl = AuthorizeTokenUrl,
+				ClientId = ClientId,
+				RequestedScope = "unifeed openid offline_access",
+			});
+
+			_currentToken = await authService.Authenticate();
+
+			Log($"Authentication complete, starting Unifeed");
+			StartUnifeed();
+		}
+
+		private void StartUnifeed(int? interfaceObjectId = null)
+		{
+			var accessToken = _currentToken.AccessToken;
+
+			var downloadUrl = SharedCode.Web.HttpExtensions.Build(UnifeedBaseUrl, new NameValueCollection()
+			{
+				{ "accessToken", accessToken },
+				{ "interface", 32.ToString() },
+				{ "interfaceType", "DOWNLOAD" },
+				{ "interfaceName", "lokaal apparaat (download)" },
+				{ "uobApplication", "Revit" },
+				{ "uobApplicationVersion", "2018" },
+				{ "interfaceObjectId", interfaceObjectId?.ToString() },
+			}).ToString();
+
+			Log($"Link to initiate download: {downloadUrl}");
+
+			var url = SharedCode.Web.HttpExtensions.Build(UnifeedBaseUrl, new NameValueCollection()
+			{
+				{ "accessToken", accessToken },
+				{ "interface", 32.ToString() },
+				{ "interfaceType", "JSONGET" },
+				{ "interfaceName", "UOB DemoApplication" },
+				{ "hookUrl", UnifeedHookUrl },
+			}).ToString();
+			browser.Navigate(url);
+			return;
+		}
+
+		private async void Browser_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+		{
+			// Restart situation
+			if (!string.IsNullOrEmpty(_currentToken.RefreshToken))
+			{
+				// Refresh token. Normally this is not needed for every call, only when the token is expired.
+				// Only possible when offline_access scope is honored
+				RefreshToken();
+			}
+
+			Log($"Navigating to: {e.Url}");
+			if (e.Url.Scheme == UnifeedSchemeName)
+			{
+				e.Cancel = true;
+				Log($"Interfaced! {e.Url}");
+
+				// Get id from data
+				var queryString = HttpUtility.ParseQueryString(e.Url.Query);
+				string data = queryString["json"];
+				var interfaceId = JsonConvert.DeserializeAnonymousType(data, new { Id = 0L }).Id;
+				Log($"Retrieved id for interface object: {interfaceId}");
+
+				// Call interface webservice
+				var url = SharedCode.Web.HttpExtensions.Build($"{ApiBaseUrl}/json/UOB/Interface", new NameValueCollection()
+				{
+					{ "id", interfaceId.ToString() },
+				}).ToString();
+
+				Log($"Calling service url: {url}");
+
+				var interfaceObjectJson = SharedCode.WebService.WebServiceHelper.GetJson(url, _currentToken.AccessToken);
+				Log($"Retrieved interface object: {Newtonsoft.Json.Linq.JToken.Parse(interfaceObjectJson).ToString(Formatting.Indented)}");
+
+				var interfaceObject = JsonConvert.DeserializeObject<SharedCode.Models.Interface>(interfaceObjectJson);
+
+				// Post the interfaceObject back
+				var postUrl = SharedCode.Web.HttpExtensions.Build($"{ApiBaseUrl}/json/UOB/Interface").ToString();
+				var output = await SharedCode.WebService.WebServiceHelper.PostJson(postUrl, _currentToken.AccessToken, interfaceObjectJson);
+
+				if (int.TryParse(output, out var newInterfaceId))
+				{
+					// Restart Unifeed
+					StartUnifeed(newInterfaceId); // browser.Refresh();
+				}
+				else
+				{
+					// Restart Unifeed
+					StartUnifeed(); // browser.Refresh();
+				}
+			}
+		}
+
+		private void Browser_Navigated(object sender, System.Windows.Forms.WebBrowserNavigatedEventArgs e)
+		{
+			// Log($"Navigated to: {e.Url}");
+		}
+
+		private void Log(string tekst)
+		{
+			logBox.Text += $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {tekst} {Environment.NewLine}";
+			System.Diagnostics.Debug.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {tekst} {Environment.NewLine}");
+		}
+
+		private void RefreshToken()
+		{
+			Log($"Refreshing tokens. Old token retrieved: {_currentToken.TokenIssued.ToString("yyyyMMdd_HHmmss")}");
+			var query = SharedCode.Web.HttpExtensions.BuildQuerystring(new NameValueCollection()
+				{
+					{ "client_id", ClientId },
+					{ "grant_type", "refresh_token" },
+					{ "refresh_token", _currentToken.RefreshToken },
+				}).ToString();
+
+			_currentToken = SharedCode.Authentication.TokenService.RetrieveToken(AuthorizeTokenUrl, query);
+
+			Log($"Refreshing tokens. New token retrieved: {_currentToken.TokenIssued.ToString("yyyyMMdd_HHmmss")}");
+		}
+	}
+}
